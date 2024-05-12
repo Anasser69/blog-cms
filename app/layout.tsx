94928c52129d86093276a66692559fbf19172f11
ahmed nasser <ahmed.timo189@gmail.com>
2024-05-12 20:52:37 +0300

    first util sanity

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter,Fira_Code } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
+import { ThemeProvider } from "next-themes";
+import { Provider } from "./utils/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
+const FiraCode = Fira_Code({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +20,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${FiraCode.className} h-full bg-amber-50 text-indigo-950 dark:bg-slate-950 dark:text-amber-50 dark:selection:bg-purple-500`}>
+        <Provider>
+          <Navbar />
+          <main className="mx-auto max-w-5xl px-6">{children}</main>
+        </Provider>
+      </body>
     </html>
   );
 }
